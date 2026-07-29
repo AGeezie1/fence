85a23928c732f62b520a39d936c7c2970fdafdd2
Asa Gee <asamingogee1@gmail.com>
2026-07-28 23:40:19 -0400

    map changes yooo
    commit before i stop for the night

--- fence.py
+++ fence.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import json
 
 # --- Settings ---
 SCREEN_WIDTH = 800
@@ -8,11 +9,37 @@ FPS = 60
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Pygame Starter Template")
+pygame.display.set_caption("fence")
 clock = pygame.time.Clock()
 
-box = pygame.Rect(00, 300, 800, 300)
+with open("fencing.json", "r") as f:
+    JSON = json.load(f)
 
+
+print(JSON["name"])
+
+
+
+def lines(x, y, width, height, count):
+    base = pygame.Rect(x, y, width, height)
+
+    line_list = []
+    spacing = width / count
+
+    for i in range(count):
+        start_pos = (x + i * spacing, y)
+        end_pos = (x + i * spacing, y + height)
+        line_list.append((start_pos, end_pos))
+
+    return base, line_list
+
+box = pygame.Rect(0, 300, 800, 300)
+strip = pygame.Rect(20, 320, 760, 75)
+
+ignore_this_line = pygame.Rect(20,320,4,75) #to cover up an extra line on the strip
+
+box, line_positions = lines(0, 300, 800, 300, 14)
+strip, line_positions2 = lines(20, 320, 760, 75, 6)
 # --- Main Loop ---
 running = True
 while running:
@@ -23,7 +50,27 @@ while running:
     # --- Drawing ---
     screen.fill((162,124,91))
 
+    #BASE
     pygame.draw.rect(screen, (92,64,51), box)
+
+    #BASE LINES
+    for start, end in line_positions:
+        pygame.draw.line(screen, (102,74,61), start, end, 2)
+
+    #STRIP
+    pygame.draw.rect(screen, (255,255,255), strip) 
+    #STRIP LINES
+    
+    for start, end in line_positions2:
+        pygame.draw.line(screen, (225,225,225), start, end, 4)
+
+
+    
+
+
+    pygame.draw.rect(screen, (255,255,255), ignore_this_line)
+
+
 
 
     pygame.display.flip()  # update the screen
